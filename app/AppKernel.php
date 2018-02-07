4e318859a67d9437493447c2cb18a0576ad6f475
David Teruel <d_teruel@hotmail.com>
2018-02-07 01:36:20 +0100

    Add endpoint to get last N tweets of an username or empty response if user does not exists

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new \Renus\LastTweetBundle\RenusLastTweetBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/AppBundle/Controller/UsersController.php
+++ src/AppBundle/Controller/UsersController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Renus\LastTweetBundle\Entity\Tweet;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -11,13 +12,17 @@ class UsersController extends FOSRestController
     /**
      * @Route("/users/{id}", defaults={"id" = 1})
      * @Method({"GET"})
+     *
+     * @param $id
+     *
+     * @return array
      */
     public function indexAction($id)
     {
         $data = array('Usuarios' => array(
             array(
-                'nombre'   => 'Víctor',
-                'Apellido' => 'Robles'
+                'nombre'   => 'Jhon',
+                'Apellido' => 'Doe'
             ),
             array(
                 'nombre'   => 'Antonio',
@@ -25,5 +30,35 @@ class UsersController extends FOSRestController
             )));
 
         return $data;
+    }
+
+    /**
+     *
+     * @Route("/user/tweets/{username}/{number}", defaults={"username" = "DavidTeruel22", "number" = 20})
+     * @Method({"GET"})
+     *
+     * @param $username
+     * @param int $number
+     *
+     * @return array
+     */
+    public function getUserLastTweetsAction($username, $number)
+    {
+        try{
+            $twitterService = $this->get('renus.twitter');
+            $tweets = $twitterService->getLastTweets($username, $number);
+
+            $onlyText = array();
+            /** @var Tweet $tweet */
+            foreach ($tweets as $tweet){
+                $value = array('tweet' => $tweet->getText());
+
+                array_push($onlyText, $value);
+            }
+
+            return array($username => $onlyText);
+        }catch (\Exception $exception){
+            return array();
+        }
     }
 }
